9802c6f18757aadd8c66c45acbafc8237a73c6de
kalistos1 <ucktech1@gmail.com>
2023-07-30 15:38:34 +0100

    added more apps

---
+++ crypto_payment/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CryptoPaymentConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'crypto_payment'
---
+++ gis_MAPPING/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GisMappingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gis_MAPPING'
