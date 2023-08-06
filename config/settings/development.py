baac46c2213387e64d7d79853177c3a4413955b7
kalistos1 <ucktech1@gmail.com>
2023-08-06 17:15:27 +0100

    resolved issues #1

--- config/settings/development.py
+++ config/settings/development.py
@@ -7,10 +7,22 @@ ALLOWED_HOSTS = []
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'clearvfarms',
+        'USER': 'postgres',
+        'PASSWORD': 'kal15t05',
+        'HOST': '127.0.0.1',  # or the IP address of your PostgreSQL server
+        'PORT': '5432',       # PostgreSQL's default port is 5432
     }
 }
 
